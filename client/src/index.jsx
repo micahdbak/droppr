efe4681b661b00b1e2b46ae9b2c03ff13751f347
Micah Baker <micahb923@gmail.com>
2024-03-23 17:17:56 -0700

    Create droppr interface

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+// see exports from ./droppr/index.js; can be used as droppr.X
+import * as droppr from './droppr/index.js';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
