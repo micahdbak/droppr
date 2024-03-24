2810a7dd3ca9d9c893463874763b190c83b01a9b
Nakul Bansal <nakulbansal727@gmail.com>
2024-03-24 00:36:47 -0700

    Updated Header

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -3,14 +3,18 @@ import React from 'react';
 export function Header() {
   return (
     <div>
-      <nav className="bg-neutral-100 shadow-sm">
-        <div className="flex flex-wrap items-center justify-between p-4">
+      <nav className="bg-neutral-50 border-neutral-200">
+        <div className="flex flex-wrap items-center justify-between py-3 px-4">
           <div className="flex items-center justify-between ml-24">
             <p className="text-3xl text-slate-950 font-bold">droppr.</p>
           </div>
           <div className="flex items-center justify-between font-medium">
-            <p className="mr-16 font-medium">Receive</p>
-            <p className="mr-24 font-medium">Drop</p>
+            <button className="mr-8 font-medium rounded-md bg-neutral-100 hover:bg-neutral-200 hover:cursor-pointer px-4 py-3 transition ease-in-out hover:scale-110">
+              Receive
+            </button>
+            <button className="mr-24 font-medium rounded-md bg-neutral-100 hover:bg-neutral-200 hover:cursor-pointer px-4 py-3 transition ease-in-out hover:scale-110">
+              Drop
+            </button>
           </div>
         </div>
       </nav>
