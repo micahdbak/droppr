8f6e42a46d1158af18f61f99056dcf8226320bb7
JohnnyDeng6 <jzydeng@gmail.com>
2024-03-24 00:45:33 -0700

    receiver.jsx implemented

--- client/src/pages/receiver.jsx
+++ client/src/pages/receiver.jsx
@@ -1,20 +1,67 @@
-import React, { useEffect } from 'react';
+// receiver App
+
+import React, { useState, useEffect } from 'react';
 
 import * as droppr from '../interface';
 
-export function Receiver(props) {
-  const { id } = props; // the drop id
+const statusInterval = 100; // 100ms
 
-  // on page load
+export function Receiver(props) {
+  //const [view, setView] = useState('droppr'); //use for header
+  const [status, setStatus] = useState('Waiting...');
+  const [downloadHref, setDownloadHref] = useState('');
+  const [downloadName, setDownloadName] = useState('');
+
+  const { id } = props;
+
+  // run on page load
   useEffect(() => {
-    droppr.receive(id, (update) => {
-      console.log(update);
-    });
+    // check statuses every 100ms
+    const checkStatusInterval = setInterval(() => {
+      const bytes = droppr.getBytes();
+      const speed = droppr.getSpeed(); // Bpms == kBps
+      const size = droppr.getSize();
+      const percentage = Math.round((100 * bytes) / fileInfo.size);
+
+      // set status to be a summary of received bytes
+      setStatus(`Received ${bytes} of ${size} bytes (${speed} kBps) (${percentage}%).`);
+    }, statusInterval);
+
+    // read transferid from URL
+
+    if (id) {
+
+      droppr.receive(id, (update) => {
+        // stop updating status
+        clearInterval(checkStatusInterval);
+        setStatus('');
+        if (update.download) {
+          setDownloadHref(update.download.href); // <a href={downloadHref}>... // set React state
+          setDownloadName(update.download.name); // ...{downloadName}</a> // set React state
+        }
+ 
+        setStatus(update.status); // set React state
+      });
+    } else {
+      // pass
+    }
+
+    return () => {
+      // clear intervals
+      clearInterval(checkStatusInterval);
+    };
   }, []);
 
+  useEffect(() => {
+    console.log(downloadName);
+  }, [downloadName]);
+
   return (
-    <div>
-      <p1 className="text-3xl">Reciever Page</p1>
-    </div>
+    <>
+      <p>{status}</p>
+      <a href={downloadHref} download={downloadName}>
+        Download
+      </a>
+    </>
   );
 }
