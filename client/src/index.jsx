586e16652eb2dacc3fac33efab6f9cb521ffb04a
Nakul Bansal <nakulbansal727@gmail.com>
2024-03-23 20:59:21 -0700

    Initial Structure

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,22 +3,14 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 
-<<<<<<< Updated upstream
-import { App } from './App.jsx';
-=======
 import { Main } from './main.jsx'
 import * as droppr from './droppr/index.js';
->>>>>>> Stashed changes
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-<<<<<<< Updated upstream
-    <App />
-=======
     <BrowserRouter>
       <Main />
     </BrowserRouter>
->>>>>>> Stashed changes
   </React.StrictMode>,
 );
