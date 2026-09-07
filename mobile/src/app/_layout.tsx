3b57b8e9b71c6fa8b08741f015d9bca88f8a08ec
James Chuong <jcjameschuong@gmail.com>
2026-09-07 10:48:43 -0700

    Remove template code

--- mobile/src/app/_layout.tsx
+++ mobile/src/app/_layout.tsx
@@ -1,18 +1,15 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from "expo-router";
-import * as SplashScreen from "expo-splash-screen";
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 import { useColorScheme } from "react-native";
 
-import { AnimatedSplashOverlay } from "@/components/animated-icon";
-import AppTabs from "@/components/app-tabs";
-
-SplashScreen.preventAutoHideAsync();
-
-export default function TabLayout() {
+export default function RootLayout() {
   const colorScheme = useColorScheme();
+
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <AnimatedSplashOverlay />
-      <AppTabs />
+      <Stack />
+      <StatusBar style="auto" />
     </ThemeProvider>
   );
 }
